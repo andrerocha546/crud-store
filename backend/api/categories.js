e348de750e9d49ec7e2c75967176f378ad3ce864
andrerocha546 <41653025+andrerocha546@users.noreply.github.com>
2020-10-09 16:44:39 -0300

    Adicionando metodo get da api de categorias

--- backend/api/categories.js
+++ backend/api/categories.js
@@ -11,5 +11,11 @@ module.exports = app => {
             .catch(err => res.status(500).send(err))
     }
 
-    return { save }
+    const get = (req, res) => {
+        app.db('products_categories')
+            .then(categories => res.json(categories))
+            .catch(err => res.status(500).send(err))
+    }
+
+    return { save, get }
 }
--- backend/config/routes.js
+++ backend/config/routes.js
@@ -3,4 +3,5 @@ module.exports = app => {
         .post(app.api.products.save)
     app.route('/categories')
         .post(app.api.categories.save)
+        .get(app.api.categories.get)
 }
