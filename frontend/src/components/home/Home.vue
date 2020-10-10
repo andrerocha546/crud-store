e04f2d2ffd5dd022c57074d7389e84f26c05f137
andrerocha546 <41653025+andrerocha546@users.noreply.github.com>
2020-10-09 22:24:27 -0300

    Refatorando layout do card do produto e leyout da Home

--- frontend/src/components/home/Home.vue
+++ frontend/src/components/home/Home.vue
@@ -32,4 +32,7 @@ export default {
 </script>
 
 <style>
+    .home {
+        display: flex;
+    }
 </style>
--- frontend/src/components/product/ProductItem.vue
+++ frontend/src/components/product/ProductItem.vue
@@ -20,7 +20,7 @@ export default {
 <style>
     .product-item-info {
         display: grid;
-        grid-template-rows: 80% 10% 10%; 
+        grid-template-rows: 70% 15% 15%; 
         grid-template-areas:
             'img'
             'title'
@@ -29,7 +29,7 @@ export default {
         align-items: center;
         border-radius: 7px;
         box-shadow: 0 0 .5em rgba(0, 0, 0, 0.2);
-        margin: 20px;
+        margin: 10px;
         width: 300px;
         height: 400px;
     }
@@ -41,6 +41,7 @@ export default {
     .product-item-info h3 {
         grid-area: title;
         text-decoration: none;
+        text-align: center;
         font-size: 1.4rem;
         font-weight: 100;
         color: #8E8E8E;
