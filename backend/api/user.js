749b0714c09f1b6b4578e3b7263cda74e97b73dc
andrerocha546 <41653025+andrerocha546@users.noreply.github.com>
2020-10-12 15:33:22 -0300

    Aplicando função de validação no metodo save da api de usuários

--- backend/api/user.js
+++ backend/api/user.js
@@ -1,6 +1,8 @@
 const bcrypt = require('bcrypt-nodejs')
 
 module.exports = app => {
+    const { existsOrError } = app.api.validation
+
     const encryptPassword = password => {
         const salt = bcrypt.genSaltSync(10)
         return bcrypt.hashSync(password, salt)
@@ -8,9 +10,17 @@ module.exports = app => {
 
     const save = (req, res) => {
         const user = { ...req.body }
-
+        
+        try {
+            existsOrError(user.name, 'Nome não informado')
+            existsOrError(user.email, 'E-mail não informado')
+            existsOrError(user.password, 'Senha não informada')
+        } catch(msg) {
+            res.status(400).send(msg)
+        }
+        
         user.password = encryptPassword(user.password)
-
+        
         app.db('users')
             .insert(user)
             .then(_ => res.status(204).send())
